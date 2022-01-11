58932597f097db4a516c2d07b365770746b1c2af
Julien <j.haigron@rubika-edu.com>
2022-01-11 16:58:57 +0100

    UI
    Menu

---
+++ LOVE_UnityMethodo/Assets/MenuManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void Play()
+    {
+        Debug.Log("load level");
+        //SceneManager.LoadScene("Level");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("quit app");
+        Application.Quit();
+    }
+}
