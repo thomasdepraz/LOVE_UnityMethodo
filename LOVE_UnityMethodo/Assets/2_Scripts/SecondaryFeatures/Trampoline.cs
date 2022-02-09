9365b704a9a8c1f2c5c6dd27f4398751b92a0902
Thomas Depraz <t.deprazdepland@gmail.com>
2022-02-09 12:03:48 +0100

    feat : added slow zone and trampoline

---
+++ LOVE_UnityMethodo/Assets/2_Scripts/SecondaryFeatures/Trampoline.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    public float jumpForceFactor;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            GameManager.Instance.player.VerticalMovement(true, jumpForceFactor);
+        }
+    }
+}
